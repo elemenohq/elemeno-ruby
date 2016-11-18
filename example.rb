7f00c648c7a124a9e6db7b568da54ff78df3017c
Chris Erwin <cdnchris@gmail.com>
2016-11-17 22:44:42 -0500

    stubbing in basic request wrapper class

---
+++ example.rb
@@ -0,0 +1,5 @@
+require "elemeno"
+
+elemeno = Elemeno::Client.new('598cba62-28c4-11e6-9eca-e7db6722f18d')
+
+elemeno.hello
--- lib/elemeno.rb
+++ lib/elemeno.rb
@@ -1,5 +1,23 @@
 require "elemeno/version"
+require "faraday"
 
 module Elemeno
-  # Your code goes here...
+	class Client
+
+		def initialize(api_key)
+			@conn = Faraday.new :url => 'http://api.elemeno.io/v1/', :headers => {'Authorization' => api_key}
+		end
+
+		def hello
+			puts @api_key
+			puts Faraday::VERSION
+			puts Faraday::default_adapter
+
+
+			response = @conn.get 'singles'
+			puts response.status
+			puts response.body
+		end
+
+	end
 end
