4ba2ee60eb946f1e729f647cdd2862fdfe174b54
Chris Erwin <cdnchris@gmail.com>
2016-11-30 11:50:43 -0500

    fleshing out ruby request wrapper

--- lib/elemeno.rb
+++ lib/elemeno.rb
@@ -1,23 +1,74 @@
 require "elemeno/version"
 require "faraday"
+require "json"
 
 module Elemeno
 	class Client
 
+		@@singleBase = 'singles/'
+		@@collectionBase = 'collections/'
+
 		def initialize(api_key)
-			@conn = Faraday.new :url => 'http://api.elemeno.io/v1/', :headers => {'Authorization' => api_key}
+			@conn = Faraday.new :url => 'http://api.elemeno.dev/v1/', :headers => {'Authorization' => api_key}
 		end
 
-		def hello
-			puts @api_key
-			puts Faraday::VERSION
-			puts Faraday::default_adapter
+		private def get(path, query = nil)
+			response = @conn.get do |req|
+				req.url path
+				if query
+					req.params = query
+				end
+			end
 
-
-			response = @conn.get 'singles'
-			puts response.status
-			puts response.body
+			return JSON.parse(response.body)
 		end
 
+		private def getQuery(options = nil, allow = false)
+			query = Hash.new
+
+			if options && options.is_a?(Hash)
+				if options.has_key?(:filters) && allow.is_a?(Array) && allow.include?('filters')
+					query['filters'] = options[:filters].to_json
+				end
+				if options.has_key?(:sort) && allow.is_a?(Array) && allow.include?('sort')
+					query['sort'] = options[:sort].to_json
+				end
+				if options.has_key?(:page)
+					query['page'] = options[:page].to_json
+				end
+				if options.has_key?(:size)
+					query['size'] = options[:size].to_json
+				end
+				if options.has_key?(:byId)
+					query['byId'] = (options[:byId].is_a?(TrueClass) || options[:byId] == 'true') ? true : false
+				end
+			end
+
+			return query
+		end
+
+		def getSingles(options = nil)
+			return get(@@singleBase, getQuery(options, ['sort']))
+		end
+
+		def getSingle(singleSlug)
+			return get(@@singleBase + singleSlug)
+		end
+
+		def getCollections(options = nil)
+			return get(@@collectionBase, getQuery(options, ['sort']))
+		end
+
+		def getCollection(collectionSlug)
+			return get(@@collectionBase + collectionSlug)
+		end
+
+		def getCollectionItems(collectionSlug, options = nil)
+			return get(@@collectionBase + collectionSlug + '/items', getQuery(options, ['filters', 'sort']))
+		end
+
+		def getCollectionItem(collectionSlug, itemSlug, options = nil)
+			return get(@@collectionBase + collectionSlug + '/items/' + itemSlug, getQuery(options, ['sort']))
+		end
 	end
 end
